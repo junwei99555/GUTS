69ad30be81c0b870c1c3def55b2b95101f1ea910
junwei9955 <junwei10255@gmail.com>
2022-02-13 20:57:34 +0800

    commit 2

--- server.py
+++ server.py
@@ -32,31 +32,66 @@ def getMorning():
 
 @app.route("/api/getWeatherData",methods=['GET'])
 def apiGetWeather():
-    location = "Europe"
+    dataList = []
 
+    location = "Frankfurt"
     complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
     api_link = requests.get(complete_api_link)
     api_data = api_link.json()
 
-    #create python object to store data
     data = {
         "temp": "%0.2f" % ((api_data['main']['temp']) - 273.15),
         "desc": api_data['weather'][0]['description'],
-        "hmdt": "%0.2f" % (api_data['main']['humidity']),
-        "wind_speed": "%0.2f" % (api_data['wind']['speed']),
         "date_time": datetime.now(pytz.timezone('GMT'))
     }
+    dataList.append(data)
+
+    location = "Glasgow"
+    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+    api_link = requests.get(complete_api_link)
+    api_data = api_link.json()
+
+    data = {
+        "temp": "%0.2f" % ((api_data['main']['temp']) - 273.15),
+        "desc": api_data['weather'][0]['description'],
+        "date_time": datetime.now(pytz.timezone('GMT'))
+    }
+    dataList.append(data)
+
+    location = "London"
+    complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+    api_link = requests.get(complete_api_link)
+    api_data = api_link.json()
+
+    data = {
+        "temp": "%0.2f" % ((api_data['main']['temp']) - 273.15),
+        "desc": api_data['weather'][0]['description'],
+        "date_time": datetime.now(pytz.timezone('GMT'))
+    }
+    dataList.append(data)
 
     # convert into JSON:
-    return jsonify(data)
+    return jsonify(dataList)
 
 @app.route("/api/getNewsData",methods=['GET'])
 def apiGetNews():
     news = GoogleNews(period='1d')
-    news.get_news('UK')
-    result = news.result()
 
-    result = result[0:6]
+    news.get_news('Frankfurt')
+    result1 = news.result()
+    result1 = result1[0:3]
+
+    news.clear()
+    news.get_news('Glasgow')
+    result2 = news.result()
+    result2 = result2[0:3]
+
+    news.clear()
+    news.get_news('London')
+    result3 = news.result()
+    result3 = result3[0:3]
+
+    result = result1 + result2 + result3
 
     # convert into JSON:
     return jsonify(result)
